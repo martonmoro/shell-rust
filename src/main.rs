215140913d44af7a73f31534e58785a723232f2f
Marton Moro <moro.marton@gmail.com>
2024-12-03 21:43:08 +0100

    exit 0

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
 
 fn main() {
     loop {
@@ -11,8 +12,15 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        // Invalid command
-        println!("{}: command not found", input.trim());
-        io::stdout().flush().unwrap();
+        let input_trimmed = input.trim();
+
+        match input_trimmed {
+            "exit 0" => process::exit(0),
+            _ => {
+                // Invalid command
+                println!("{}: command not found", input_trimmed);
+                io::stdout().flush().unwrap();
+            }
+        }
     }
 }
