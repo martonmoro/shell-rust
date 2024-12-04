6b1b337f5c5e54f11fb1d8ef84eb7794d36bd309
Marton Moro <moro.marton@gmail.com>
2024-12-04 15:58:17 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{collections::HashSet, process};
+use std::{env, fs, process};
 
 fn main() {
+    let path = env::var("PATH").unwrap();
     loop {
-        let mut builtins = HashSet::new();
-        builtins.insert("exit");
-        builtins.insert("echo");
-        builtins.insert("type");
+        // let mut builtins = HashSet::new();
+        // builtins.insert("exit");
+        // builtins.insert("echo");
+        // builtins.insert("type");
+
+        let builtins = ["exit", "echo", "type"];
 
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -19,24 +22,32 @@ fn main() {
 
         let input_trimmed = input.trim();
 
-        let (command, arguments) = match input_trimmed.split_once(' ') {
-            Some((command, args)) => (command, args),
-            None => (input_trimmed, ""),
-        };
+        let argv = input_trimmed.split_whitespace().collect::<Vec<&str>>();
 
-        match command {
-            "exit" if arguments == "0" => process::exit(0),
-            "echo" => println!("{}", arguments),
+        match argv[0] {
+            "exit" => process::exit(0),
+            "echo" => println!("{}", argv[1..].join(" ")),
             "type" => {
-                if builtins.contains(arguments) {
-                    println!("{} is a shell builtin", arguments);
+                if argv.len() != 2 {
+                    println!("type: expected 1 argument, got {}", argv.len() - 1);
+                }
+                let cmd = argv[1];
+                if builtins.contains(&cmd) {
+                    println!("{} is a shell builtin", cmd);
                 } else {
-                    println!("{}: not found", arguments)
+                    let split = &mut path.split(":");
+                    if let Some(path) =
+                        split.find(|path| fs::metadata(format!("{}/{}", path, cmd)).is_ok())
+                    {
+                        println!("{cmd} is {path}/{cmd}")
+                    } else {
+                        println!("{}: not found", cmd)
+                    }
                 }
             }
             _ => {
                 // Invalid command
-                println!("{}: command not found", command);
+                println!("{}: command not found", input_trimmed);
                 io::stdout().flush().unwrap();
             }
         }
